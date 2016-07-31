f6fb85c15fe2100bc637539983d4ada7d22960fc
Justyna Bruniaux <justyna.lipska74@wp.pl>
2016-08-01 01:09:27 +0200

    header_sliding_with_jquery

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".name").slideDown(2000);
+});
+
+$(function() {
+    $('#datetimepicker12').datetimepicker({
+        inline: true,
+        sideBySide: true
+    });
+});
+
+/*============================================================================*/
+ $(document).ready(function(){
+    $('.carousel').carousel({
+      interval: 3000
+    })
+  });
